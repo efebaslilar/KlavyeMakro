ae588bec4c852fe6ac77e0c935a61e300f7cf121
Efe Baslılar <hopa_efe_08@hotmail.com>
2023-06-20 18:06:09 +0300

    Add project files.

---
+++ KlavyeMakro/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using WindowsInput;
+
+InputSimulator inputSimulator = new InputSimulator();
+string replyText = "";
+int replyTime;
+
+
+Console.WriteLine("Tekrar edilecek metini girin:");
+replyText = Console.ReadLine();
+
+if (string.IsNullOrEmpty(replyText))
+{
+    Environment.Exit(0);
+}
+
+Console.WriteLine("Kaç ms'de bir tekrar etmesi gerektiğini girin:");
+if (Int32.TryParse(Console.ReadLine(), out replyTime))
+{
+    while (true)
+    {
+        KeyboardPress();
+        Thread.Sleep(replyTime);
+    }
+}
+else
+{
+    Console.WriteLine("Lütfen sadece sayı giriniz!!");
+}
+
+
+ void KeyboardPress()
+{
+    inputSimulator.Keyboard.TextEntry(replyText);
+}
+
